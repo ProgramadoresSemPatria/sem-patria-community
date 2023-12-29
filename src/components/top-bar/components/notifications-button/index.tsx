365cbd04a06e06249bfb960ee40f2f6a0f603393
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-28 21:33:07 -0300

    refactor: updating NotificationsButton component tree
    + fixing hydration errors due to nested buttons at PopoverTrigger

--- src/components/top-bar/components/notifications-button/index.tsx
+++ src/components/top-bar/components/notifications-button/index.tsx
@@ -1,14 +1,25 @@
+import { Icons } from '@/components/icons'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover'
 import React from 'react'
 import NotificationsButtonTrigger from './notifications-button-trigger'
-import NotificationsList from './notifications-list'
-
-// import { Container } from './styles';
+import NotificationsListContent from './notifications-list-content'
 
 const NotificationsButton: React.FC = () => {
   return (
-    <NotificationsList>
-      <NotificationsButtonTrigger />
-    </NotificationsList>
+    <Popover>
+      <PopoverTrigger className="hover:bg-accent p-2 rounded-md">
+        <NotificationsButtonTrigger>
+          <Icons.notifications />
+        </NotificationsButtonTrigger>
+      </PopoverTrigger>
+      <PopoverContent className="p-0">
+        <NotificationsListContent />
+      </PopoverContent>
+    </Popover>
   )
 }
 
--- src/components/top-bar/components/notifications-button/notifications-button-trigger.tsx
+++ src/components/top-bar/components/notifications-button/notifications-button-trigger.tsx
@@ -1,26 +1,26 @@
 'use client'
-import { Icons } from '@/components/icons'
-import { Button } from '@/components/ui/button'
+
 import { useNotifications } from '@/hooks/use-notifications'
 import React, { useMemo } from 'react'
+interface NotificationsButtonTriggerProps {
+  children: React.ReactNode
+}
 
-const NotificationsButtonTrigger: React.FC = () => {
+const NotificationsButtonTrigger: React.FC<NotificationsButtonTriggerProps> = ({
+  children
+}: NotificationsButtonTriggerProps) => {
   const { data } = useNotifications()
   const hasNotifications = useMemo(() => {
     return data?.length > 0
   }, [data])
 
   return (
-    <Button variant="ghost" className="gap-x-2 items-center">
-      {hasNotifications ? (
-        <div className="relative">
-          <Icons.notifications />
-          <div className="absolute top-[1px] right-[1px] w-[9px] h-[9px] bg-red-600 rounded-full border" />
-        </div>
-      ) : (
-        <Icons.notifications />
+    <div className="relative">
+      {children}
+      {hasNotifications && (
+        <div className="absolute top-[1px] right-[1px] w-[9px] h-[9px] bg-red-600 rounded-full border" />
       )}
-    </Button>
+    </div>
   )
 }
 

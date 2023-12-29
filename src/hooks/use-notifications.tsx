3ef5f4c17a48ad16f3a194dc2d99b114dbef9a23
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-29 20:31:00 -0300

    feat: adding course status mutation at notifications custom hook

--- src/hooks/use-notifications.tsx
+++ src/hooks/use-notifications.tsx
@@ -1,7 +1,44 @@
 'use client'
 
+import { toast } from '@/components/ui/use-toast'
 import { api } from '@/lib/api'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
+
+export const useApproveOrDeclineCourseMutation = ({
+  courseId,
+  type
+}: {
+  courseId: string
+  type: 'approve' | 'decline'
+}) => {
+  return useMutation({
+    mutationKey: ['approveCourse'],
+    mutationFn: async () => {
+      await api.put(`/api/courses/pending?type=${type}&courseId=${courseId}`)
+    },
+    onSuccess: () => {
+      if (type === 'approve') {
+        toast({
+          title: 'Course was approved with success',
+          description: 'It can now be seen by the community.'
+        })
+      } else {
+        toast({
+          title: 'Course was declined',
+          description: 'The course request will be deleted.',
+          variant: 'destructive'
+        })
+      }
+    },
+    onError: () => {
+      toast({
+        title: 'Something went wrong on course approval',
+        description: 'Please try again later.',
+        variant: 'destructive'
+      })
+    }
+  })
+}
 
 export const useNotifications = () => {
   return useQuery({

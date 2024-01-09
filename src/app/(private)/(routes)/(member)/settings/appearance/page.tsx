bc1d3226e60d8d4d55bdaadab2eee8ea7948c2a3
bebossi <bernardobossi77@gmail.com>
2024-01-09 20:33:13 +0000

    refactor: add responsivennes to pages and components

--- src/app/(private)/(routes)/(member)/settings/appearance/page.tsx
+++ src/app/(private)/(routes)/(member)/settings/appearance/page.tsx
@@ -3,7 +3,7 @@ import { AppearanceForm } from './components/appearance-form'
 const AppearancePage = () => {
   return (
     <>
-      <div className="space-y-6 w-2/3 mt-4">
+      <div className="space-y-6  mt-4 ">
         <AppearanceForm />
       </div>
     </>

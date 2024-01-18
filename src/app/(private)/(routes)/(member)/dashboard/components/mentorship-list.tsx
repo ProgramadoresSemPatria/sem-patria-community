416c538fbdec5f954ddcbbea7c94d2709a67a3c2
Carlos Lopes <carloslopessf@gmail.com>
2024-01-18 11:46:07 -0300

    fix: images path and design

--- src/app/(private)/(routes)/(member)/dashboard/components/mentorship-list.tsx
+++ src/app/(private)/(routes)/(member)/dashboard/components/mentorship-list.tsx
@@ -1,12 +1,4 @@
-// import genesis from '@/assets/genesis.PNG'
-// import hefesto from '@/assets/hefesto.PNG'
-// import hermes from '@/assets/hermes.PNG'
-// import apollo from '@/assets/apollo.PNG'
-// import prometeu from '@/assets/prometeu.PNG'
-// import zeus from '@/assets/zeus.PNG'
-// import hades from '@/assets/hades.PNG'
-
-import Image from 'next/image'
+import { Badge } from '@/components/ui/badge'
 import {
   Carousel,
   CarouselContent,
@@ -14,36 +6,38 @@ import {
   CarouselNext,
   CarouselPrevious
 } from '@/components/ui/carousel'
+import { carouselItemsMentorshipList } from '@/lib/constants'
+import Image from 'next/image'
 import Link from 'next/link'
-import { CarouselItemsMentorshipList } from '@/lib/constants'
 
 const MentorshipList = () => {
   return (
     <div className="py-6">
-      <h1 className="text-xl font-bold mb-4">
-        Mentorship Program{' '}
-        <span className="bg-gray-800 rounded-lg text-sm p-[6px] ml-4">
-          Comming Soon
-        </span>{' '}
-      </h1>
+      <div className="flex items-center gap-x-4 mb-4">
+        <h1 className="text-xl font-bold">Mentorship Program</h1>
+        <Badge className="m-0">Comming Soon</Badge>
+      </div>
+
       <Carousel className="mx-6">
         <CarouselContent>
-          {CarouselItemsMentorshipList.map(item => (
+          {carouselItemsMentorshipList.map(item => (
             <CarouselItem
               key={item.src}
               className="group relative basis-1/4 md:basis-1/3 lg:basis-1/5 xl:basis-[14.2857143%]"
             >
               <Link href={item.href}>
                 <Image
-                  className="rounded-lg group-hover:opacity-20"
+                  className="rounded-lg group-hover:opacity-40"
                   src={item.src}
                   alt={item.title + 'img'}
                   width={200}
                   height={200}
                 />
-                <div className="absolute hidden group-hover:block top-1/3 left-1/3  text-lg font-black">
-                  {item.phase}{' '}
-                  <span className="text-purple-600">{item.title}</span>
+                <div className="absolute hidden top-0 bottom-0 left-0 right-0 group-hover:flex flex-col justify-center text-center">
+                  <span>{item.phase}</span>
+                  <span className="text-purple-600 font-bold">
+                    {item.title}
+                  </span>
                 </div>
               </Link>
             </CarouselItem>
--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -1,4 +1,11 @@
 import { type CarouselItemsMentorshipListProps } from './types'
+import Genesis from '@/assets/genesis.png'
+import Hefesto from '@/assets/hefesto.png'
+import Hermes from '@/assets/hermes.png'
+import Apollo from '@/assets/apollo.png'
+import Prometeu from '@/assets/prometeu.png'
+import Zeus from '@/assets/zeus.png'
+import Hades from '@/assets/hades.png'
 
 export const appConfig = {
   name: 'Sem pátria - Community',
@@ -31,46 +38,46 @@ export const settingOptions = [
   }
 ]
 
-export const CarouselItemsMentorshipList: CarouselItemsMentorshipListProps[] = [
+export const carouselItemsMentorshipList: CarouselItemsMentorshipListProps[] = [
   {
     href: '/',
-    src: '/assets/genesis.PNG',
+    src: Genesis.src,
     phase: 'Phase 1 ',
     title: 'GENESIS'
   },
   {
     href: '/',
-    src: '/assets/hefesto.PNG',
+    src: Hefesto.src,
     phase: 'Phase 2 ',
     title: 'HEFESTO'
   },
   {
     href: '/',
-    src: '/assets/hermes.PNG',
+    src: Hermes.src,
     phase: 'Phase 3 ',
     title: 'HERMES'
   },
   {
     href: '/',
-    src: '/assets/apollo.PNG',
+    src: Apollo.src,
     phase: 'Phase 4 ',
     title: 'APOLLO'
   },
   {
     href: '/',
-    src: '/assets/prometeu.PNG',
+    src: Prometeu.src,
     phase: 'Phase 5 ',
     title: 'PROMETEU'
   },
   {
     href: '/',
-    src: '/assets/zeus.PNG',
+    src: Zeus.src,
     phase: 'Phase 6 ',
     title: 'ZEUS'
   },
   {
     href: '/',
-    src: '/assets/hades.PNG',
+    src: Hades.src,
     phase: 'Phase 7 ',
     title: 'HADES'
   }

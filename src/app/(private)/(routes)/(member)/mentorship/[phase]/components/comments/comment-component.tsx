481dfe6da77613c05ce40460c432919b18f1dc35
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-11 08:51:38 -0300

    refactor: adding mutations handler to comment-component

--- src/app/(private)/(routes)/(member)/mentorship/[phase]/components/comments/comment-component.tsx
+++ src/app/(private)/(routes)/(member)/mentorship/[phase]/components/comments/comment-component.tsx
@@ -1,8 +1,14 @@
 import { Icons } from '@/components/icons'
 import { Button } from '@/components/ui/button'
+import { toast } from '@/components/ui/use-toast'
+import { api } from '@/lib/api'
 import { useAuth } from '@clerk/nextjs'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { EditorContent, useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
 import { format } from 'date-fns'
 import Image from 'next/image'
+import { useReducer } from 'react'
 import { AdminActions } from './admin-actions'
 import { type Comment } from './comments'
 
@@ -11,6 +17,7 @@ interface CommentComponentProps {
 }
 export const CommentComponent = ({ comment }: CommentComponentProps) => {
   const { userId } = useAuth()
+  const queryClient = useQueryClient()
 
   const getStringFromDate = (date: string) => {
     const commentDate = new Date(date)
@@ -32,6 +39,74 @@ export const CommentComponent = ({ comment }: CommentComponentProps) => {
       return `${hours} hours ago`
     } else {
       return `${minutes} minutes ago`
+    }
+  }
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        heading: {
+          HTMLAttributes: {
+            class: 'text-xl font-bold'
+          }
+        },
+        bulletList: {
+          HTMLAttributes: {
+            class: 'list-disc pl-4'
+          }
+        },
+        orderedList: {
+          HTMLAttributes: {
+            class: 'list-decimal pl-4'
+          }
+        }
+      })
+    ],
+    editable: false,
+    content: comment.comment
+  })
+
+  const { mutateAsync } = useMutation({
+    mutationKey: ['like-comment'],
+    mutationFn: async () => {
+      return await api.put(`/api/comment/like/${comment.id}`)
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['comments'] })
+    },
+    onError: error => {
+      toast({
+        title: 'An error occurred.',
+        description: 'Unable to like the comment',
+        variant: 'destructive'
+      })
+      console.log(error)
+    }
+  })
+
+  const [likeState, dispatch] = useReducer(
+    (state: { likes: number; liked: boolean }, action: { type: string }) => {
+      switch (action.type) {
+        case 'LIKE':
+          return { ...state, liked: true, likes: state.likes + 1 }
+        case 'UNLIKE':
+          return { ...state, liked: false, likes: state.likes - 1 }
+        default:
+          return state
+      }
+    },
+    {
+      liked: comment.likes.includes(userId ?? ''),
+      likes: comment.likes.length
+    }
+  )
+
+  const handleLike = async () => {
+    try {
+      dispatch({ type: likeState.liked ? 'UNLIKE' : 'LIKE' })
+      await mutateAsync()
+    } catch (error) {
+      dispatch({ type: likeState.liked ? 'LIKE' : 'UNLIKE' })
     }
   }
 
@@ -60,20 +135,25 @@ export const CommentComponent = ({ comment }: CommentComponentProps) => {
         </div>
         <AdminActions commentId={comment.id} />
       </div>
-      <p className="text-sm">{comment.comment}</p>
+      <EditorContent editor={editor} className="text-sm" />
       <div className="flex items-center w-fit space-x-1 mt-2 font-bold text-slate-600 text-sm">
-        <Button variant="ghost" size="icon" className="group rounded-full ">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="group rounded-full"
+          onClick={handleLike}
+        >
           <Icons.upVote
-            data-userliked={comment.likes.includes(userId ?? '')}
+            data-userliked={likeState.liked}
             className="h-5 data-[userliked=true]:text-violet-900 group-hover:text-white "
             strokeWidth={2}
           />
         </Button>
         <p
-          data-userliked={comment.likes.includes(userId ?? '')}
+          data-userliked={likeState.liked}
           className="leading-4 data-[userliked=true]:text-violet-900"
         >
-          {comment.likes.length}
+          {likeState.likes}
         </p>
       </div>
     </div>

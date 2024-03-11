48f0423dd4d84f80ae143425cf700511623af8bf
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-11 08:51:07 -0300

    refactor: adding mutations handler to admin-actions component

--- src/app/(private)/(routes)/(member)/mentorship/[phase]/components/comments/admin-actions.tsx
+++ src/app/(private)/(routes)/(member)/mentorship/[phase]/components/comments/admin-actions.tsx
@@ -1,4 +1,5 @@
 import { Icons } from '@/components/icons'
+import { AlertModal } from '@/components/modals/alert-modal'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
@@ -6,26 +7,68 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
-import { useMemo } from 'react'
+import { toast } from '@/components/ui/use-toast'
+import { api } from '@/lib/api'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useCallback, useMemo, useState } from 'react'
 
 interface AdminActionsProps {
   commentId: string
 }
 export const AdminActions = ({ commentId }: AdminActionsProps) => {
+  const queryClient = useQueryClient()
+  const [isAlertModalOpen, setIsAlertModalOpen] = useState(false)
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationKey: ['delete-comment'],
+    mutationFn: async () => {
+      return await api.delete(`/api/comment/`, {
+        data: { commentId }
+      })
+    },
+    onSuccess: async () => {
+      toast({
+        title: 'Comment deleted',
+        description: 'The comment has been deleted'
+      })
+      await queryClient.refetchQueries({ queryKey: ['comments'] })
+    },
+    onError: error => {
+      toast({
+        title: 'An error occurred.',
+        description: 'Unable to delete the comment',
+        variant: 'destructive'
+      })
+      console.log(error)
+    }
+  })
+
+  const handleDelete = useCallback(async () => {
+    await mutateAsync()
+  }, [mutateAsync])
+
   const adminActions = useMemo(() => {
     return [
       {
         label: 'Delete comment',
         icon: <Icons.trash className="h-4" />,
-        onClick: () => {
-          console.log('delete')
-        }
+        onClick: handleDelete,
+        isPending
       }
     ]
-  }, [])
+  }, [handleDelete, isPending])
 
   return (
     <div className="flex gap-2 items-center ">
+      <AlertModal
+        isOpen={isAlertModalOpen}
+        description="Are you sure you want to delete this comment?"
+        onClose={() => {
+          setIsAlertModalOpen(false)
+        }}
+        onConfirm={handleDelete}
+        loading={isPending}
+      />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" size="icon" className="group rounded-full">
@@ -34,7 +77,12 @@ export const AdminActions = ({ commentId }: AdminActionsProps) => {
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-fit">
           {adminActions.map(action => (
-            <DropdownMenuItem key={action.label}>
+            <DropdownMenuItem
+              key={action.label}
+              onClick={() => {
+                setIsAlertModalOpen(true)
+              }}
+            >
               {action.icon}
               {action.label}
             </DropdownMenuItem>

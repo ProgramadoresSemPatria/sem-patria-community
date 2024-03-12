6a15c3989fac862911560be9e5e43d318d2d5921
bebossi <bernardobossi77@gmail.com>
2024-03-12 11:15:46 +0000

    refactor: remove unused logic

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,4 +1,3 @@
-import { getCheckMembership } from '@/actions/auth/get-check-membership'
 import { appRoutes } from '@/lib/constants'
 import { currentUser } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
@@ -10,10 +9,6 @@ export default async function PrivateLayout({
 }) {
   const user = await currentUser()
   if (!user?.username) return redirect(appRoutes.signIn)
-
-  const isMemberOfOrg = await getCheckMembership(user.username)
-
-  if (!isMemberOfOrg) return redirect(appRoutes.root)
 
   return <div>{children}</div>
 }

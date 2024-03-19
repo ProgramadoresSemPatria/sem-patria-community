8e70a2cc179b81de98ace6d662cfd34bfb2b971a
Lucas Yule <lyrma@cin.ufpe.br>
2024-03-19 16:12:18 -0300

    fix: fixing eslint error at sendEmailWithPassword

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -88,7 +88,7 @@ export async function POST(req: NextRequest) {
     }
   })
 
-  sendEmailWithPassword(email, newUserPassword)
+  await sendEmailWithPassword(email, newUserPassword)
 
   return NextResponse.json(user)
 }

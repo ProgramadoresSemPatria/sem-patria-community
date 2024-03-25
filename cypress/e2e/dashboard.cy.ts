145a207e3b0d4e5210a228e7d5f1b776bc1f5b47
bebossi <bernardobossi77@gmail.com>
2024-03-25 15:22:01 +0000

    test: should go to the /mentoship page by clicking the mentorship button

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -15,4 +15,16 @@ describe('Dashboard', () => {
         cy.contains('CMS')
       })
   })
+
+  it('Should go to the /mentoship page by clicking the mentorship button in the sidebar', () => {
+    cy.visit('/dashboard', {
+      failOnStatusCode: false
+    })
+    cy.get('[href="/mentorship"] > .font-medium')
+      .click()
+      .then(() => {
+        cy.contains('Mentorship')
+        cy.contains('Browse all our community content.')
+      })
+  })
 })

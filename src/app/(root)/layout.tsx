04880aee311a351e32dbb8f41a543a6a11b42adc
bebossi <bernardobossi77@gmail.com>
2024-04-01 21:45:41 +0100

    feat: remove auth for public routes

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,14 +1,14 @@
-import { appRoutes } from '@/lib/constants'
-import { currentUser } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
+// import { appRoutes } from '@/lib/constants'
+// import { currentUser } from '@clerk/nextjs'
+// import { redirect } from 'next/navigation'
 
 export default async function SetupLayout({
   children
 }: {
   children: React.ReactNode
 }) {
-  const user = await currentUser()
-  if (!user?.username) return redirect(appRoutes.signIn)
+  // const user = await currentUser()
+  // if (!user?.username) return redirect(appRoutes.signIn)
 
   return <>{children}</>
 }

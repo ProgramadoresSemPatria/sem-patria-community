7451d6047f8d842572dc713f4be087984c56cee2
Bernardo Bossi Garcia de Paiva <bernardobossi77@gmail.com>
2024-04-23 21:40:53 +0100

    feat; add PostActions and set title to be always a h1 element

--- src/app/(private)/(routes)/(member)/forum/components/post.tsx
+++ src/app/(private)/(routes)/(member)/forum/components/post.tsx
@@ -10,6 +10,8 @@ import { type FC, useRef } from 'react'
 import StarterKit from '@tiptap/starter-kit'
 import { useRouter } from 'next/navigation'
 import PostLike from './post-likes'
+import avatarImg from '@/assets/avatar.png'
+import { PostActions } from './post-actions'
 
 interface PostProps {
   post: ExtendedPost
@@ -56,49 +58,49 @@ const Post: FC<PostProps> = ({
     content: post.content as string
   })
 
-  const editorTitle = useEditor({
-    extensions: [
-      StarterKit.configure({
-        heading: {
-          HTMLAttributes: {
-            class: 'text-xl font-bold'
-          }
-        },
-        bulletList: {
-          HTMLAttributes: {
-            class: 'list-disc pl-4'
-          }
-        },
-        orderedList: {
-          HTMLAttributes: {
-            class: 'list-decimal pl-4'
-          }
-        }
-      })
-    ],
-    editable: false,
-    content: post.title as string
-  })
-
+  // const editorTitle = useEditor({
+  //   extensions: [
+  //     StarterKit.configure({
+  //       heading: {
+  //         HTMLAttributes: {
+  //           class: 'text-2xl font-bold'
+  //         }
+  //       },
+  //       bulletList: {
+  //         HTMLAttributes: {
+  //           class: 'list-disc pl-4'
+  //         }
+  //       },
+  //       orderedList: {
+  //         HTMLAttributes: {
+  //           class: 'list-decimal pl-4'
+  //         }
+  //       }
+  //     })
+  //   ],
+  //   editable: false,
+  //   content: post.title as string
+  // })
+  const isCurrentUserPost = post.userId === userId
   return (
     <div
-      onClick={e => {
-        e.stopPropagation()
-        router.push(`/forum/${post.id}`)
-      }}
+      // onClick={e => {
+      //   e.stopPropagation()
+      //   router.push(`/forum/${post.id}`)
+      // }}
       className="rounded-md bg-slate-900 shadow text-white hover:cursor-pointer "
     >
       <div className="px-6 py-4 flex justify-between">
         <div className="w-0 flex-1">
-          <div className="flex max-h-40 text-gray-300">
+          <div className="flex items-center max-h-40 text-gray-300">
             <Image
-              className="rounded-full w-8 mr-1"
+              className="rounded-full w-8"
               alt=""
               width={500}
               height={500}
-              src={(post.user.imageUrl as string) || ''}
+              src={(post.user.imageUrl as string) || avatarImg.src}
             />
-            <div className="flex flex-col -mt-2  ml-2">
+            <div className="flex flex-col ml-2">
               <span className="font-bold text-base">{post.user.username}</span>
               <span className="text-xs">
                 {getStringFromDate(post.createdAt.toString())} in{' '}
@@ -114,21 +116,19 @@ const Post: FC<PostProps> = ({
               </span>
             </div>
           </div>
-          {post.title && (
-            <a href={`/forum/${post.id}`}>
-              <EditorContent editor={editorTitle} />
-            </a>
-          )}
           <div
             className="relative text-sm max-h-40 w-full mt-2 overflow-clip"
             ref={pRef}
           >
-            <EditorContent editor={editor} className="text-sm" />
+            {/* <EditorContent editor={editorTitle} /> */}
+            <h1 className="font-bold text-2xl">{post.title}</h1>
+            <EditorContent editor={editor} className="text-sm max-h-[80px]" />
             {pRef.current?.clientHeight === 160 ? (
               <div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent"></div>
             ) : null}
           </div>
         </div>
+        {isCurrentUserPost && <PostActions post={post} />}{' '}
       </div>
       <div className="flex items-center">
         <PostLike

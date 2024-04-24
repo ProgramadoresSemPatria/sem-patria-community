bce2108bfd4e5feb52654438af3b9c6181f9e635
Bernardo Bossi Garcia de Paiva <bernardobossi77@gmail.com>
2024-04-24 21:32:27 +0100

    feat: add Filters Icon next to Input of create modal

--- src/app/(private)/(routes)/(member)/forum/components/create-post.tsx
+++ src/app/(private)/(routes)/(member)/forum/components/create-post.tsx
@@ -1,20 +1,33 @@
 'use client'
+import { Icons } from '@/components/icons'
 import CreatePostModal from '@/components/modals/create-post-modal'
 import { Input } from '@/components/ui/input'
 import useCreatePostModalStore from '@/hooks/modal/use-create-post'
 import React from 'react'
+import FiltersModal from '@/components/modals/filters-modal'
+import useFiltersModalStore from '@/hooks/modal/use-filters-modal'
 
 function CreatePostComponent() {
   const { isOpen, onOpen } = useCreatePostModalStore()
+  const { isOpen: isFiltersOpen, onOpen: onOpenFilters } =
+    useFiltersModalStore()
 
   return (
-    <div>
+    <div className="flex items-center justify-between py-2">
       <Input
+        className="w-[97%]"
         placeholder="What is on your mind?"
         onFocus={() => {
           onOpen()
         }}
       />
+      <Icons.settings
+        onClick={() => {
+          onOpenFilters()
+        }}
+      />
+      {isFiltersOpen && <FiltersModal />}
+
       {isOpen && <CreatePostModal />}
     </div>
   )

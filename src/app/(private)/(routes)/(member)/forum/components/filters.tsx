aebb5e3d6d0f7c0280f58d3229c6932f5a490aa6
Bernardo Bossi <bernardobossi77@gmail.com>
2024-04-27 12:33:24 +0100

    feat: apply new dynamic to filters

--- src/app/(private)/(routes)/(member)/forum/components/filters.tsx
+++ src/app/(private)/(routes)/(member)/forum/components/filters.tsx
@@ -1,45 +1,47 @@
 'use client'
 import { Icons } from '@/components/icons'
+import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel
-} from '@/components/ui/form'
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem
+} from '@/components/ui/command'
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
-} from '@/components/ui/select'
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover'
 import { useCategory } from '@/hooks/category/use-category'
-import useFiltersModalStore from '@/hooks/modal/use-filters-modal'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useRouter } from 'next/navigation'
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-
-const contentSchema = z.object({
-  orderBy: z.string(),
-  category: z.string()
-})
+import { cn } from '@/lib/utils'
+import { Check } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import React, { useState } from 'react'
 
 const ForumFilters = () => {
-  const { onClose } = useFiltersModalStore()
-  const router = useRouter()
-  const form = useForm({
-    resolver: zodResolver(contentSchema),
-    mode: 'onChange',
-    defaultValues: {
-      orderBy: '',
-      category: ''
-    }
-  })
   const { categories } = useCategory()
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const [openCategory, setOpenCategory] = useState(false)
+  const [openOrderBy, setOpenOrderBy] = useState(false)
+  const orderByOptions = [
+    {
+      id: 'datenew',
+      value: 'New in'
+    },
+    {
+      id: 'dateold',
+      value: 'Old ones'
+    },
+    {
+      id: 'likes',
+      value: 'Most likes'
+    }
+  ]
+  const [categoryName, setCategoryName] = useState('All')
+  const [orderBy, setOrderBy] = useState('')
 
   const categoryOptions =
     categories && categories.length > 0
@@ -52,89 +54,135 @@ const ForumFilters = () => {
         ]
       : []
 
-  const onSubmit = () => {
-    router.push(
-      `/forum?category=${form.getValues().category}${
-        form.getValues().orderBy ? `&orderBy=${form.getValues().orderBy}` : ''
-      }`
+  const handleSelect = (selectedId: string) => {
+    const selectedOption = orderByOptions.find(
+      option => option.id === selectedId
     )
-    onClose()
+    if (selectedOption) {
+      setOrderBy(selectedOption.value)
+      router.push(
+        `/forum?category=${searchParams.get('category')}&orderBy=${
+          selectedOption.id
+        }`
+      )
+    }
   }
+
   return (
-    <Form {...form}>
-      <form
-        className="flex flex-col gap-4"
-        onSubmit={form.handleSubmit(onSubmit)}
+    <div className="flex py-2 gap-x-2">
+      <Popover open={openCategory} onOpenChange={setOpenCategory}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={openCategory}
+            className="flex w-fit justify-between border-dashed hover:cursor-pointer gap-x-2"
+          >
+            {'Category...'}
+            <Icons.arrowUpDown className=" h-4 w-4 shrink-0 opacity-50" />
+            {categoryName && (
+              <Badge className="bg-slate-900 text-white">
+                {searchParams.get('category') || categoryName}
+              </Badge>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="Search category..." />
+            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandGroup>
+              {categoryOptions.map(category => (
+                <CommandItem
+                  className="line-clamp-1"
+                  key={category.name}
+                  value={category.name}
+                  onSelect={currentValue => {
+                    setCategoryName(
+                      currentValue === category.name ? '' : category.name
+                    )
+                    setOpenCategory(false)
+                    router.push(
+                      `/forum?category=${category.name}${
+                        searchParams.get('orderBy')
+                          ? `&orderBy=${searchParams.get('orderBy')}`
+                          : ''
+                      }`
+                    )
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      'mr-2 h-4 w-4',
+                      categoryName === category.name
+                        ? 'opacity-100'
+                        : 'opacity-0'
+                    )}
+                  />
+                  {category.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+
+      <Popover open={openOrderBy} onOpenChange={setOpenOrderBy}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={openOrderBy}
+            className="flex w-fit justify-between border-dashed hover:cursor-pointer gap-x-2"
+          >
+            {'Order by...'}
+            <Icons.arrowUpDown className=" h-4 w-4 shrink-0 opacity-50" />
+            {orderBy && (
+              <Badge className="bg-slate-900  text-white">{orderBy}</Badge>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandGroup>
+              {orderByOptions.map(orderByOption => (
+                <CommandItem
+                  className="line-clamp-1"
+                  key={orderByOption.id}
+                  value={orderByOption.id}
+                  onSelect={currentValue => {
+                    handleSelect(currentValue)
+                    setOpenOrderBy(false)
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      'mr-2 h-4 w-4',
+                      orderBy === orderByOption.value
+                        ? 'opacity-100'
+                        : 'opacity-0'
+                    )}
+                  />
+                  {orderByOption.value}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+
+      <Button
+        onClick={() => {
+          router.push(`/forum?category=All`)
+          setCategoryName('All')
+          setOrderBy('')
+        }}
+        variant="ghost"
       >
-        <FormField
-          control={form.control}
-          name="category"
-          render={({ field }) => (
-            <FormItem className="w-full">
-              <FormLabel>Category</FormLabel>
-              <Select
-                onValueChange={field.onChange}
-                value={field.value}
-                defaultValue={field.value}
-              >
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue
-                      defaultValue={field.value}
-                      placeholder="Select a category"
-                    />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  {categoryOptions &&
-                    categoryOptions.length > 0 &&
-                    categoryOptions.map(category => (
-                      <SelectItem key={category.id} value={category.name}>
-                        {category.name}
-                      </SelectItem>
-                    ))}
-                </SelectContent>
-              </Select>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="orderBy"
-          render={({ field }) => (
-            <FormItem className="w-full">
-              <FormLabel>OrderBy</FormLabel>
-              <Select
-                // disabled={isPending}
-                onValueChange={field.onChange}
-                value={field.value}
-                defaultValue={field.value}
-              >
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue
-                      defaultValue={field.value}
-                      placeholder="Order by"
-                    />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="date">Date</SelectItem>
-                  <SelectItem value="likes">Most likes</SelectItem>
-                </SelectContent>
-              </Select>
-            </FormItem>
-          )}
-        />
-        <Button
-          type="submit"
-          disabled={!form.formState.isValid || form.formState.isSubmitting}
-          className="self-end w-fit bg-slate-800 text-white gap-1 hover:bg-slate-900"
-        >
-          Apply filters
-        </Button>
-      </form>
-    </Form>
+        Reset
+        <Icons.close className="ml-2" size={15} />
+      </Button>
+    </div>
   )
 }
 

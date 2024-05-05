b73785c4b4184b96aefb791cf33a6307606fba4e
Bernardo Bossi <bernardobossi77@gmail.com>
2024-05-05 11:55:03 +0100

    feat: implement redis on routes

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -2,7 +2,7 @@ import prismadb from '@/lib/prismadb'
 import { auth } from '@clerk/nextjs'
 import { type NextRequest, NextResponse } from 'next/server'
 import { z } from 'zod'
-
+import { redis } from '@/lib/redis'
 export async function POST(req: NextRequest) {
   try {
     const { userId } = auth()
@@ -24,6 +24,12 @@ export async function POST(req: NextRequest) {
         userId
       }
     })
+    await redis.zadd('posts', {
+      score: new Date(post.createdAt).getMilliseconds(),
+      member: JSON.stringify(post)
+    })
+
+    // await redis.lpush('posts', JSON.stringify(post))
 
     return NextResponse.json(post)
   } catch (error) {
@@ -70,24 +76,56 @@ export async function GET(req: Request) {
         default:
           orderByClause = { createdAt: 'desc' }
       }
+    } else {
+      orderByClause = { createdAt: 'desc' }
     }
 
     if (categoryName !== 'All') {
       whereClause = { category: { name: categoryName } }
     }
+    const cachedPosts = await redis.zrange('posts', 0, -1)
+    console.log('cached postss', cachedPosts)
+    let posts
+    if (cachedPosts.length > 0) {
+      posts = cachedPosts.map(post => JSON.parse(post as string))
+      // .filter(post => post.category.name === categoryName)
+    }
+    if (!posts || posts.length < Number(limit)) {
+      posts = await prismadb.post.findMany({
+        take: parseInt(limit),
+        skip: (parseInt(page) - 1) * parseInt(limit),
+        orderBy: orderByClause,
+        include: {
+          category: true,
+          likes: true,
+          user: true,
+          comments: true
+        },
+        where: whereClause
+      })
 
-    const posts = await prismadb.post.findMany({
-      take: limit ? parseInt(limit) : undefined,
-      skip: limit ? (parseInt(page) - 1) * parseInt(limit) : undefined,
-      orderBy: orderByClause,
-      include: {
-        category: true,
-        likes: true,
-        user: true,
-        comments: true
-      },
-      where: whereClause
-    })
+      // Optionally refresh the cache
+      posts.forEach(
+        async post =>
+          await redis.zadd('posts', {
+            score: new Date(post.createdAt).getTime(),
+            member: JSON.stringify(post)
+          })
+      )
+    }
+
+    // const posts = await prismadb.post.findMany({
+    //   take: limit ? parseInt(limit) : undefined,
+    //   skip: limit ? (parseInt(page) - 1) * parseInt(limit) : undefined,
+    //   orderBy: orderByClause,
+    //   include: {
+    //     category: true,
+    //     likes: true,
+    //     user: true,
+    //     comments: true
+    //   },
+    //   where: whereClause
+    // })
 
     return new Response(JSON.stringify(posts))
   } catch (error) {

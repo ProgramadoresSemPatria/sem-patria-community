3774b90160e8e56674bb51c82231f3dabe57f0ab
Carlos Lopes <carloslopessf@gmail.com>
2024-05-14 18:51:47 -0300

    fix: gen pass route

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,12 @@ import { authMiddleware } from '@clerk/nextjs'
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/api/webhooks(.*)', '/api/auth/(.*)', '/api/uploadthing(.*)']
+  publicRoutes: [
+    '/api/webhooks(.*)',
+    '/api/auth/(.*)',
+    '/api/uploadthing(.*)',
+    '/set-password/(.*)'
+  ]
 })
 
 export const config = {

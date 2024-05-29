ed849d6fb5fa8bd2b5c485ac7be1c1e8233f2a9e
Igor Alves <igorsk89@gmail.com>
2024-05-29 13:19:54 -0300

    feat: reset challenges by time

--- src/components/checklist/index.tsx
+++ src/components/checklist/index.tsx
@@ -8,15 +8,16 @@ import { appRoutes } from '@/lib/constants'
 import { ScrollArea } from '../ui/scroll-area'
 import { useChecklist } from '@/hooks/checklist/use-checklist'
 import { ChecklistItem } from './components/checklist-item'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import { type ChallengeItem } from '@/hooks/checklist/type'
+import { addDays, addMonths, addWeeks, isAfter, parseISO } from 'date-fns'
 
 type ChecklistProps = {
   isWidget?: boolean
 }
 
 export const Checklist = ({ isWidget = false }: ChecklistProps) => {
-  const { challenges } = useChecklist()
+  const { challenges, toggleComplete } = useChecklist()
 
   const completedItems = useMemo(
     () =>
@@ -33,6 +34,56 @@ export const Checklist = ({ isWidget = false }: ChecklistProps) => {
         .reduce((sum, length) => sum + length, 0),
     [challenges]
   )
+
+  const resetChallengesByTime = () => {
+    const now = new Date()
+
+    const { daily, weekly, monthly } = challenges
+
+    daily.forEach(challenge => {
+      if (challenge.updatedAt) {
+        const updatedAt = parseISO(challenge.updatedAt)
+        const nextDay = addDays(updatedAt, 1)
+
+        if (isAfter(now, nextDay)) {
+          toggleComplete('daily', challenge.id, false)
+        }
+      } else {
+        challenge.updatedAt = now.toISOString()
+      }
+    })
+
+    weekly.forEach(challenge => {
+      if (challenge.updatedAt) {
+        const updatedAt = parseISO(challenge.updatedAt)
+        const nextWeek = addWeeks(updatedAt, 1)
+
+        if (isAfter(now, nextWeek)) {
+          toggleComplete('weekly', challenge.id, false)
+        }
+      } else {
+        challenge.updatedAt = now.toISOString()
+      }
+    })
+
+    monthly.forEach(challenge => {
+      if (challenge.updatedAt) {
+        const updatedAt = parseISO(challenge.updatedAt)
+        const nextMonth = addMonths(updatedAt, 1)
+
+        if (isAfter(now, nextMonth)) {
+          toggleComplete('monthly', challenge.id, false)
+        }
+      } else {
+        challenge.updatedAt = now.toISOString()
+      }
+    })
+  }
+
+  useEffect(() => {
+    resetChallengesByTime()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   if (isWidget) {
     return (

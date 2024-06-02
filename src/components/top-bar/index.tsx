e72b49d8ffd1f7b0b7ffd138fdf7b3cc7d23d583
carlosdevv <carloslopessf@gmail.com>
2024-06-02 11:05:02 -0300

    feat: feedback link

--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -8,14 +8,17 @@ import { LogoutButton } from './logout-button'
 import { MobileButton } from './mobile-button'
 import NotificationsButton from './notifications-button/index'
 
-const TopBar = async () => {
+const feedbackLink =
+  'https://docs.google.com/forms/d/e/1FAIpQLSc_UTMhjo2-HH6XovqWajs5RKj_2LQMCq2kz8itV-NcheU8oA/viewform'
+
+const TopBar = () => {
   return (
     <div className="flex h-[60px] items-center justify-end border-b border-slate-6 px-6">
       <MainLogo isMobile />
       <div className="flex items-center gap-3">
         <FeedbackButton />
         <Button variant="outline">
-          <Link href="" className="flex items-center gap-x-2">
+          <Link href={feedbackLink} className="flex items-center gap-x-2">
             <span>Give your feedback</span>
             <Icons.redirect className="h-4 w-4 text-violet-600" />
           </Link>

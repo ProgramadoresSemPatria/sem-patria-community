858de9852d642a811dd296d94c54d9dea502605f
Lucas Yule <lyrma@cin.ufpe.br>
2024-06-02 14:50:40 -0300

    refactor: extending send-email-with-link to receive template and subject

--- src/actions/auth/send-email-with-link.ts
+++ src/actions/auth/send-email-with-link.ts
@@ -1,4 +1,3 @@
-import { emailTemplate } from '@/lib/constants'
 import { type User } from '@prisma/client'
 import jwt from 'jsonwebtoken'
 import { Resend } from 'resend'
@@ -14,16 +13,22 @@ const generateToken = (user: User) => {
   )
 }
 
+type EmailTemplateFunction = (user: User, url: string) => string
+
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export const sendEmailWithLink = async (user: User) => {
+export const sendEmailWithLink = async (
+  user: User,
+  template: EmailTemplateFunction,
+  subject: string
+) => {
   const token = generateToken(user)
   const url =
     process.env.NODE_ENV === 'development'
       ? `${process.env.BASE_URL_DEVELOPMENT}/set-password/${token}`
       : `${process.env.BASE_URL_PRODUCTION}/set-password/${token}`
 
-  const htmlTemplate = emailTemplate(user, url)
+  const htmlTemplate = template(user, url)
   const userEmail =
     process.env.NODE_ENV === 'development'
       ? process.env.DEVELOPER_EMAIL
@@ -37,7 +42,7 @@ export const sendEmailWithLink = async (user: User) => {
   await resend.emails.send({
     from: fromEmail,
     to: userEmail ?? user.email,
-    subject: 'Atualize sua senha - Borderless Coding Community',
+    subject,
     html: htmlTemplate
   })
 }

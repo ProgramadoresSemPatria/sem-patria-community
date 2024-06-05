47f1e05b055370de4c01cfacb5a102085d1489f7
Igor Alves <igorsk89@gmail.com>
2024-06-05 12:25:21 -0300

    feat: improve appearance form

--- src/app/(private)/(routes)/(member)/profile/appearance/components/appearance-form.tsx
+++ src/app/(private)/(routes)/(member)/profile/appearance/components/appearance-form.tsx
@@ -36,12 +36,8 @@ export const AppearanceForm = () => {
   function onSubmit(data: AppearanceFormValues) {
     setTheme(data.theme)
     toast({
-      title: 'You submitted the following values:',
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      )
+      title: '🎨 Theme updated',
+      description: `Changed to ${data.theme}.`
     })
   }
 

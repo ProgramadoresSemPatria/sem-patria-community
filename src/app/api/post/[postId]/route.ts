439020dc415979f6b92be67545577688e25fb4f1
Bernardo Bossi <bernardobossi77@gmail.com>
2024-06-09 12:01:50 +0100

    fix: update route to admin delete posts

--- src/app/api/post/[postId]/route.ts
+++ src/app/api/post/[postId]/route.ts
@@ -15,14 +15,21 @@ export async function DELETE(
 
     if (!postId) return new NextResponse('Post id is required', { status: 400 })
 
-    await prismadb.post.delete({
-      where: {
-        id: postId,
-        userId
-      }
-    })
-
-    // if (!post) return new NextResponse('Post not found', { status: 404 })
+    const user = await prismadb.user.findFirst({ where: { id: userId } })
+    if (user?.role.includes('Admin')) {
+      await prismadb.post.delete({
+        where: {
+          id: postId
+        }
+      })
+    } else {
+      await prismadb.post.delete({
+        where: {
+          id: postId,
+          userId
+        }
+      })
+    }
 
     return new NextResponse('Post deleted', { status: 200 })
   } catch (error) {

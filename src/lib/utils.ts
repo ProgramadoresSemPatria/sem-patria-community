cdc15b02eeb19ac900672756141b2f7fe219eee2
Bernardo Bossi <bernardobossi77@gmail.com>
2024-06-11 21:16:07 +0100

    feat: add bg color to estagiario

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,6 +11,7 @@ export function cn(...inputs: ClassValue[]) {
 export const validateUserLevelColor = (level: string): string => {
   const color: Record<string, string> = {
     unknown: 'bg-gray-800 hover:bg-gray-800',
+    estagiario: 'bg-violet-700 hover:bg-violet-700',
     junior: 'bg-violet-800 hover:bg-violet-800',
     pleno: 'bg-violet-900 hover:bg-violet-900',
     senior: 'bg-violet-950 hover:bg-violet-950'

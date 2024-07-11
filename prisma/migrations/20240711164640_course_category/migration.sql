5777daab6636d2ea1149f2d253edaae1e69f00f5
Bernardo Bossi <bernardobossi77@gmail.com>
2024-07-11 22:14:31 +0100

    feat: update schema for a course support 1+ categories

---
+++ prisma/migrations/20240711164640_course_category/migration.sql
@@ -0,0 +1,16 @@
+-- DropForeignKey
+ALTER TABLE "Course" DROP CONSTRAINT "Course_categoryId_fkey";
+
+-- CreateTable
+CREATE TABLE "CourseCategory" (
+    "courseId" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+
+    CONSTRAINT "CourseCategory_pkey" PRIMARY KEY ("courseId","categoryId")
+);
+
+-- AddForeignKey
+ALTER TABLE "CourseCategory" ADD CONSTRAINT "CourseCategory_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CourseCategory" ADD CONSTRAINT "CourseCategory_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/migrations/20240711165100_remove_category_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `categoryId` on the `Course` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Course" DROP COLUMN "categoryId";

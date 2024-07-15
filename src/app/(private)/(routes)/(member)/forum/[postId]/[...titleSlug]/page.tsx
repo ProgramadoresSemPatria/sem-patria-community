216bddaf5f68323e0253eef558b4474a5f5a2824
Bernardo Bossi <bernardobossi77@gmail.com>
2024-07-15 20:04:56 +0100

    fix: metadata

--- src/app/(private)/(routes)/(member)/forum/[postId]/[...titleSlug]/page.tsx
+++ src/app/(private)/(routes)/(member)/forum/[postId]/[...titleSlug]/page.tsx
@@ -15,6 +15,8 @@ import PostLike from '../../components/post-likes'
 import CommentSection from '../components/comment-section'
 import EditPostButton from '../components/edit-post-button'
 import PostCommentsLink from '../components/post-comments-link'
+import Head from 'next/head'
+import slugify from 'slugify'
 
 type PostPageProps = {
   params: {
@@ -22,24 +24,18 @@ type PostPageProps = {
     titleSlug: string
   }
 }
-// const generateMetadaData = async ({ params }: PostPageProps) => {
-//   if (!params?.postId) {
-//     console.error('Params or postId is missing')
-//     return null
-//   }
+const generateMetadaData = async post => {
+  const titleSlug = slugify(post.title)
+  const dessc = JSON.parse(post.content)
 
-//   console.log(params)
-//   const post = await getPost(params.postId)
-//   console.log(post)
-
-//   return {
-//     title: post?.title,
-//     description: String(post?.content).substring(0, 150),
-//     alternates: {
-//       canonical: `http://localhost:3000/forum/${params.postId}`
-//     }
-//   }
-// }
+  return {
+    title: post?.title,
+    description: dessc.content[0].content[0].text,
+    alternates: {
+      canonical: `http://localhost:3000/forum/${post.id}/${titleSlug}`
+    }
+  }
+}
 
 export type ExtendedComment = Comment & {
   likes: Array<{
@@ -60,86 +56,94 @@ const PostPage = async ({ params }: PostPageProps) => {
   console.log(params)
   const { userId } = auth()
   const post = await getPost(params.postId)
-  // await generateMetadaData({ params: { postId: post?.id } })
+  const metadata = await generateMetadaData(post)
 
   return (
-    <DefaultLayout>
-      <Suspense fallback={'loading'}>
-        <div className="h-full flex flex-col items-center sm:items-start justify-between mt-10 w-full gap-4">
-          <BackButton isIcon={false} />
-          <h1 className="font-bold text-4xl">{post?.title}</h1>
-          <div className="flex gap-4 items-center mt-6">
-            <Image
-              src={post?.user.imageUrl || defaultAvatar}
-              alt={post?.user.username || 'User Image'}
-              width={1000}
-              height={1000}
-              className="rounded-full shadow-md w-12 h-12"
-            />
-            <div className="flex flex-col gap-1">
-              <span className="text-muted-foreground truncate text-lg">
-                <Link
-                  href="#"
-                  className="font-semibold hover:underline transition-all"
-                >
-                  {post?.user.username !== ''
-                    ? post?.user.username
-                    : post?.user.name}
-                </Link>
-              </span>
-              <span className="text-muted-foreground truncate text-xs">
-                {post?.createdAt
-                  ? format(post?.createdAt, 'MMMM dd, yyyy')
-                  : 'Date not available'}
-              </span>
-              {post?.updatedAt && (
-                <span className="text-muted-foreground truncate text-xs">
-                  Last updated at{' '}
-                  {format(post?.updatedAt, 'MM dd, yyyy, HH:mm')}
+    <>
+      <Head>
+        <title>{metadata?.title}</title>
+        <meta name="description" content={metadata?.description} />
+        <meta property="og:title" content={metadata?.title} />
+        <meta property="og:description" content={metadata?.description} />
+      </Head>
+      <DefaultLayout>
+        <Suspense fallback={'loading'}>
+          <div className="h-full flex flex-col items-center sm:items-start justify-between mt-10 w-full gap-4">
+            <BackButton isIcon={false} />
+            <h1 className="font-bold text-4xl">{post?.title}</h1>
+            <div className="flex gap-4 items-center mt-6">
+              <Image
+                src={post?.user.imageUrl || defaultAvatar}
+                alt={post?.user.username || 'User Image'}
+                width={1000}
+                height={1000}
+                className="rounded-full shadow-md w-12 h-12"
+              />
+              <div className="flex flex-col gap-1">
+                <span className="text-muted-foreground truncate text-lg">
+                  <Link
+                    href="#"
+                    className="font-semibold hover:underline transition-all"
+                  >
+                    {post?.user.username !== ''
+                      ? post?.user.username
+                      : post?.user.name}
+                  </Link>
                 </span>
+                <span className="text-muted-foreground truncate text-xs">
+                  {post?.createdAt
+                    ? format(post?.createdAt, 'MMMM dd, yyyy')
+                    : 'Date not available'}
+                </span>
+                {post?.updatedAt && (
+                  <span className="text-muted-foreground truncate text-xs">
+                    Last updated at{' '}
+                    {format(post?.updatedAt, 'MM dd, yyyy, HH:mm')}
+                  </span>
+                )}
+              </div>
+            </div>
+            <div className="flex gap-6 mt-4 px-4 text-muted-foreground w-full font-semibold text-md items-center">
+              {post && userId && (
+                <>
+                  <PostLike
+                    post={post as unknown as ExtendedPost}
+                    userId={userId}
+                    isPostPage
+                  />
+
+                  <PostCommentsLink
+                    comments={post?.comments as unknown as ExtendedComment[]}
+                    postId={post.id}
+                  />
+                </>
+              )}
+              <Separator decorative orientation="vertical" className="h-5" />
+              <p className="font-normal">at {post?.category.name}</p>
+              {post?.userId === userId && (
+                <EditPostButton
+                  postId={post?.id}
+                  title={post.title}
+                  content={post.content}
+                  categoryId={post.categoryId}
+                />
               )}
             </div>
+            <Separator />
+            <NoteEditor
+              editable={false}
+              initialValue={JSON.parse(post?.content as string)}
+              variant="readonly"
+            />
+            <Separator />
+            <CommentSection
+              comments={post?.comments as unknown as ExtendedComment[]}
+              postId={post?.id}
+            />
           </div>
-          <div className="flex gap-6 mt-4 px-4 text-muted-foreground w-full font-semibold text-md items-center">
-            {post && userId && (
-              <>
-                <PostLike
-                  post={post as unknown as ExtendedPost}
-                  userId={userId}
-                  isPostPage
-                />
-
-                <PostCommentsLink
-                  comments={post?.comments as unknown as ExtendedComment[]}
-                  postId={post.id}
-                />
-              </>
-            )}
-            <Separator decorative orientation="vertical" className="h-5" />
-            <p className="font-normal">at {post?.category.name}</p>
-            {post?.userId === userId && (
-              <EditPostButton
-                postId={post?.id}
-                title={post.title}
-                content={post.content}
-                categoryId={post.categoryId}
-              />
-            )}
-          </div>
-          <Separator />
-          <NoteEditor
-            editable={false}
-            initialValue={JSON.parse(post?.content as string)}
-            variant="readonly"
-          />
-          <Separator />
-          <CommentSection
-            comments={post?.comments as unknown as ExtendedComment[]}
-            postId={post?.id}
-          />
-        </div>
-      </Suspense>
-    </DefaultLayout>
+        </Suspense>
+      </DefaultLayout>
+    </>
   )
 }
 

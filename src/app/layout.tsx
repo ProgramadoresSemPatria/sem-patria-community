1869b38f3f3115666ad4758ec1423764eaf90af9
Bernardo Bossi <bernardobossi77@gmail.com>
2024-08-11 11:08:56 +0100

    fix: back default metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
-// import { appConfig } from '@/lib/constants'
+import { appConfig } from '@/lib/constants'
 import { cn } from '@/lib/utils'
 import { AppProviders } from '@/providers/app-provider'
 import { ClerkProvider } from '@clerk/nextjs'
 import { dark } from '@clerk/themes'
 import { Analytics } from '@vercel/analytics/react'
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-// export const metadata: Metadata = {
-//   title: {
-//     default: appConfig.name,
-//     template: `%s | ${appConfig.name}`
-//   },
-//   description: appConfig.description,
-//   icons: {
-//     icon: '/logo.svg',
-//     shortcut: '/logo.svg',
-//     apple: '/logo.svg'
-//   }
-// }
+export const metadata: Metadata = {
+  title: {
+    default: appConfig.name,
+    template: `%s | ${appConfig.name}`
+  },
+  description: appConfig.description,
+  icons: {
+    icon: '/logo.svg',
+    shortcut: '/logo.svg',
+    apple: '/logo.svg'
+  }
+}
 
 export default function RootLayout({
   children

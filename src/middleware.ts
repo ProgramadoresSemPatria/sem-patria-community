8a035a5d0a5f5079391b8d42c278a09b8d59a0d7
Bernardo Bossi <bernardobossi77@gmail.com>
2024-08-24 12:35:04 +0100

    fix: tryyyy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,81 +1,53 @@
-import { authMiddleware } from '@clerk/nextjs'
-import { NextResponse } from 'next/server'
+// import { authMiddleware } from '@clerk/nextjs'
+import { type NextRequest, NextResponse } from 'next/server'
 
 const CORS_ALLOWED_ROUTES = ['/api/og/(.*)']
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: [
-    '/api/webhooks(.*)',
-    '/api/auth/(.*)',
-    '/api/uploadthing(.*)',
-    '/set-password/(.*)',
-    '/api/password-recovery(.*)',
-    'api/og/(.*)',
-    '/forum/(.*)',
-    '/forum/(.*)/(.*)'
-  ],
-  afterAuth(auth, req) {
-    const res = NextResponse.next()
+// export default authMiddleware({
+//   publicRoutes: [
+//     '/api/webhooks(.*)',
+//     '/api/auth/(.*)',
+//     '/api/uploadthing(.*)',
+//     '/set-password/(.*)',
+//     '/api/password-recovery(.*)',
+//     'api/og/(.*)',
+//     '/forum/(.*)',
+//     '/forum/(.*)/(.*)'
+//   ]
+// })
 
-    if (
-      CORS_ALLOWED_ROUTES.some(path =>
-        new RegExp(path).test(req.nextUrl.pathname)
-      )
-    ) {
-      res.headers.set('Access-Control-Allow-Origin', '*')
-      res.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-      res.headers.set(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization'
-      )
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next()
 
-      if (req.method === 'OPTIONS') {
-        return new NextResponse(null, {
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            'Access-Control-Max-Age': '86400'
-          }
-        })
-      }
+  if (
+    CORS_ALLOWED_ROUTES.some(path =>
+      new RegExp(path).test(req.nextUrl.pathname)
+    )
+  ) {
+    res.headers.set('Access-Control-Allow-Origin', '*')
+    res.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    res.headers.set(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization'
+    )
+
+    if (req.method === 'OPTIONS') {
+      return new NextResponse(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          'Access-Control-Max-Age': '86400'
+        }
+      })
     }
-
-    return res
-  },
-  beforeAuth(req, evt) {
-    const res = NextResponse.next()
-
-    if (
-      CORS_ALLOWED_ROUTES.some(path =>
-        new RegExp(path).test(req.nextUrl.pathname)
-      )
-    ) {
-      res.headers.set('Access-Control-Allow-Origin', '*')
-      res.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-      res.headers.set(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization'
-      )
-
-      if (req.method === 'OPTIONS') {
-        return new NextResponse(null, {
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            'Access-Control-Max-Age': '86400'
-          }
-        })
-      }
-    }
-
-    return res
   }
-})
+
+  return res
+}
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']

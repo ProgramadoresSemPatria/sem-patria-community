e7bf1cf8e22accdcc22201e2c318d7d00e261d74
Bernardo Bossi <bernardobossi77@gmail.com>
2024-08-24 12:00:43 +0100

    fix: try

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
+import { type NextRequest, NextResponse } from 'next/server'
+
+const CORS_ALLOWED_ROUTES = ['/api/og/(.*)']
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -14,19 +17,37 @@ export default authMiddleware({
     '/forum/(.*)',
     '/forum/(.*)/(.*)'
   ]
-  // beforeAuth(req, evt) {
-  //   const userAgent = req.headers.get('user-agent')
-  //   if (userAgent?.includes('Discordbot')) {
-  //     console.log('let discord accesss', req)
-  //     const payload = { user: 'discord-bot' }
-  //     const secret = process.env.JWT_SECRET
-  //     const jwtToken = jwt.sign(payload, secret as string, { expiresIn: '1h' })
-
-  //     req.headers.set('authorization', `Bearer ${jwtToken}`)
-  //     return NextResponse.next()
-  //   }
-  // }
 })
+
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+
+  if (
+    CORS_ALLOWED_ROUTES.some(path =>
+      new RegExp(path).test(req.nextUrl.pathname)
+    )
+  ) {
+    res.headers.set('Access-Control-Allow-Origin', '*')
+    res.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    res.headers.set(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization'
+    )
+
+    if (req.method === 'OPTIONS') {
+      return new NextResponse(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          'Access-Control-Max-Age': '86400'
+        }
+      })
+    }
+  }
+
+  return res
+}
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']

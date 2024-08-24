5edf786af552864cce5b7a2a0376c1cd906cd268
Bernardo Bossi <bernardobossi77@gmail.com>
2024-08-24 12:19:52 +0100

    fix: tryyyy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { authMiddleware } from '@clerk/nextjs'
-import { type NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
 const CORS_ALLOWED_ROUTES = ['/api/og/(.*)']
 
@@ -47,36 +47,6 @@ export default authMiddleware({
     return res
   }
 })
-
-export function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-
-  if (
-    CORS_ALLOWED_ROUTES.some(path =>
-      new RegExp(path).test(req.nextUrl.pathname)
-    )
-  ) {
-    res.headers.set('Access-Control-Allow-Origin', '*')
-    res.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-    res.headers.set(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization'
-    )
-
-    if (req.method === 'OPTIONS') {
-      return new NextResponse(null, {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-          'Access-Control-Max-Age': '86400'
-        }
-      })
-    }
-  }
-
-  return res
-}
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']

161e600f1c05910769d333bd61d6faadb7bc0716
Bernardo Bossi <bernardobossi77@gmail.com>
2024-08-25 12:15:06 +0100

    fix: test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 import { authMiddleware } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 
-const CORS_ALLOWED_ROUTES = ['/api/og/(.*)', '/forum/(.*)', '/forum/(.*)/(.*)']
-
 export default authMiddleware({
   publicRoutes: [
     '/api/webhooks(.*)',
@@ -15,50 +13,25 @@ export default authMiddleware({
     '/forum/(.*)/(.*)'
   ],
   beforeAuth(req, evt) {
-    const res = NextResponse.next()
     const userAgent = req.headers.get('user-agent')
 
     if (userAgent && userAgent.includes('Discordbot/2.0')) {
-      console.log('Discord bot detected')
       if (req.headers.get('authorization') === null) {
-        console.log('Returning 200 instead of 401 for Discord bot')
-        return new NextResponse(null, {
+        const res = new NextResponse(null, {
           status: 200,
           headers: {
-            'Content-Type': 'text/plain'
-          }
-        })
-      }
-    }
-
-    if (
-      CORS_ALLOWED_ROUTES.some(path =>
-        new RegExp(path).test(req.nextUrl.pathname)
-      )
-    ) {
-      res.headers.set('Access-Control-Allow-Origin', '*')
-      res.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-      res.headers.set(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization'
-      )
-
-      if (req.method === 'OPTIONS') {
-        return new NextResponse(null, {
-          headers: {
+            'Content-Type': 'text/plain',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization',
             'Access-Control-Max-Age': '86400'
-          },
-          status: 200
+          }
         })
+        console.log('res', res)
+
+        return res
       }
     }
-
-    console.log('res', res)
-
-    return res
   }
 })
 

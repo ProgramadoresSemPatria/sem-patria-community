597b484cf3e3fa63e4dd05d485a9c40997e6cb04
Bernardo Bossi <bernardobossi77@gmail.com>
2024-08-27 19:51:29 +0100

    feat: show metada for post url

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,99 +1,11 @@
-// import { authMiddleware } from '@clerk/nextjs'
-// import {
-//   type NextFetchEvent,
-//   type NextRequest,
-//   NextResponse
-// } from 'next/server'
-
 import { authMiddleware } from '@clerk/nextjs/server'
-import { type NextRequest, NextResponse } from 'next/server'
+import {
+  type NextFetchEvent,
+  type NextRequest,
+  NextResponse
+} from 'next/server'
 
-// const clerkMiddleware = authMiddleware({
-//   publicRoutes: [
-//     '/api/webhooks(.*)',
-//     '/api/auth/(.*)',
-//     '/api/uploadthing(.*)',
-//     '/set-password/(.*)',
-//     '/api/password-recovery(.*)',
-//     'api/og/(.*)',
-//     '/forum/(.*)',
-//     '/forum/(.*)/(.*)'
-//   ],
-//   debug: true,
-//   beforeAuth(req, evt) {
-//     const userAgent = req.headers.get('user-agent')
-
-//     if (userAgent && userAgent.includes('Discordbot/2.0')) {
-//       if (req.headers.get('authorization') === null) {
-//         console.log('req', req)
-//         const res = new NextResponse(null, {
-//           status: 200,
-//           headers: {
-//             'Content-Type': 'text/html',
-//             'Access-Control-Allow-Origin': '*',
-//             'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-//             'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-//             'Access-Control-Max-Age': '86400'
-//           }
-//         })
-//         console.log('res', res)
-
-//         return res
-//       }
-//     }
-//     return NextResponse.next()
-//   }
-// })
-// const CORS_PATHS = ['/api/og/(.*)']
-
-// export default async function middleware(
-//   req: NextRequest,
-//   evt: NextFetchEvent
-// ) {
-//   // Apply Clerk's authMiddleware first
-//   const authResponse = clerkMiddleware(req, evt)
-
-//   // If Clerk's authMiddleware returns a response, return it immediately
-//   if (authResponse) {
-//     return await authResponse
-//   }
-
-//   // Create a new response object to modify headers
-//   const response = NextResponse.next()
-
-//   const origin = req.headers.get('origin')
-
-//   // Allow CORS only on specific paths
-//   if (CORS_PATHS.some(path => req.nextUrl.pathname.startsWith(path))) {
-//     response.headers.set('Access-Control-Allow-Origin', origin || '*')
-//     response.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-//     response.headers.set(
-//       'Access-Control-Allow-Headers',
-//       'Content-Type, Authorization'
-//     )
-//   }
-
-//   // Handle preflight CORS requests
-//   if (req.method === 'OPTIONS') {
-//     return new NextResponse(null, {
-//       headers: {
-//         'Access-Control-Allow-Origin': origin || '*',
-//         'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-//         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-//         'Access-Control-Max-Age': '86400'
-//       }
-//     })
-//   }
-
-//   return response
-// }
-
-// export const config = {
-//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
-// }
-const CORS_ALLOWED_ROUTES = ['/api/og/(.*)', '/forum/(.*)', '/forum/(.*)/(.*)']
-
-export default authMiddleware({
+const clerkMiddleware = authMiddleware({
   publicRoutes: [
     '/api/webhooks(.*)',
     '/api/auth/(.*)',
@@ -104,79 +16,73 @@ export default authMiddleware({
     '/forum/(.*)',
     '/forum/(.*)/(.*)'
   ],
-  // afterAuth(auth, req, evt) {
-  //   const a = auth.isPublicRoute
-  //   console.log('isPublicRoute', a)
-  //   if (
-  //     CORS_ALLOWED_ROUTES.some(path =>
-  //       new RegExp(path).test(req.nextUrl.pathname)
-  //     )
-  //   ) {
-  //     evt.passThroughOnException()
-  //   }
-  //   return NextResponse.next()
-  // },
-  debug: true
-  // beforeAuth(req, evt) {
-  //   const res = NextResponse.next()
+  debug: true,
+  beforeAuth(req, evt) {
+    const userAgent = req.headers.get('user-agent')
 
-  //   // Apply CORS headers if the request matches the allowed routes
-  //   if (
-  //     CORS_ALLOWED_ROUTES.some(path =>
-  //       new RegExp(path).test(req.nextUrl.pathname)
-  //     )
-  //   ) {
-  //     res.headers.set('Access-Control-Allow-Origin', '*')
-  //     res.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-  //     res.headers.set(
-  //       'Access-Control-Allow-Headers',
-  //       'Content-Type, Authorization'
-  //     )
+    if (userAgent && userAgent.includes('Discordbot/2.0')) {
+      if (req.headers.get('authorization') === null) {
+        console.log('req', req)
+        const res = new NextResponse(null, {
+          status: 200,
+          headers: {
+            'Content-Type': 'text/html',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Access-Control-Max-Age': '86400'
+          }
+        })
+        console.log('res', res)
 
-  //     if (req.method === 'OPTIONS') {
-  //       return new NextResponse(null, {
-  //         headers: {
-  //           'Access-Control-Allow-Origin': '*',
-  //           'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-  //           'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-  //           'Access-Control-Max-Age': '86400'
-  //         }
-  //       })
-  //     }
-  //   }
-  //   return NextResponse.next()
-  // }
+        return res
+      }
+    }
+    return NextResponse.next()
+  }
 })
+const CORS_PATHS = ['/api/og/(.*)']
 
-export function middleware(req: NextRequest) {
-  const res = NextResponse.next()
+export default async function middleware(
+  req: NextRequest,
+  evt: NextFetchEvent
+) {
+  // Apply Clerk's authMiddleware first
+  const authResponse = clerkMiddleware(req, evt)
 
-  // Apply CORS headers if the request matches the allowed routes
-  if (
-    CORS_ALLOWED_ROUTES.some(path =>
-      new RegExp(path).test(req.nextUrl.pathname)
-    )
-  ) {
-    res.headers.set('Access-Control-Allow-Origin', '*')
-    res.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    res.headers.set(
+  // If Clerk's authMiddleware returns a response, return it immediately
+  if (authResponse) {
+    return await authResponse
+  }
+
+  // Create a new response object to modify headers
+  const response = NextResponse.next()
+
+  const origin = req.headers.get('origin')
+
+  // Allow CORS only on specific paths
+  if (CORS_PATHS.some(path => req.nextUrl.pathname.startsWith(path))) {
+    response.headers.set('Access-Control-Allow-Origin', origin || '*')
+    response.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    response.headers.set(
       'Access-Control-Allow-Headers',
       'Content-Type, Authorization'
     )
-
-    if (req.method === 'OPTIONS') {
-      return new NextResponse(null, {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-          'Access-Control-Max-Age': '86400'
-        }
-      })
-    }
   }
 
-  return res
+  // Handle preflight CORS requests
+  if (req.method === 'OPTIONS') {
+    return new NextResponse(null, {
+      headers: {
+        'Access-Control-Allow-Origin': origin || '*',
+        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Max-Age': '86400'
+      }
+    })
+  }
+
+  return response
 }
 
 export const config = {

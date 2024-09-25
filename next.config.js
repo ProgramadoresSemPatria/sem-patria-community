0db8cc1c41226e84c2a523227724ccc330075abf
Bernardo Bossi <bernardobossi77@gmail.com>
2024-09-25 10:52:47 -0300

    feat: api key next config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
         hostname: 'utfs.io'
       }
     ]
+  },
+  env: {
+    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+    RESEND_API_KEY: process.env.RESEND_API_KEY
   }
 }
 

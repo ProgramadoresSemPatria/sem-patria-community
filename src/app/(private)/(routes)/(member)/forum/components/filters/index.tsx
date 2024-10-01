598653738912e06991bd7aa8daa772b9da3a63cf
Igor Alves <igorsk89@gmail.com>
2024-10-01 10:10:38 -0300

    feat: improve search bar responsiveness

--- src/app/(private)/(routes)/(member)/forum/components/filters/index.tsx
+++ src/app/(private)/(routes)/(member)/forum/components/filters/index.tsx
@@ -82,7 +82,7 @@ const ForumFilters = () => {
 
   return (
     <div className="flex flex-wrap py-2 gap-2 mb-4">
-      <div className="flex-grow min-w-[200px]">
+      <div className="w-full md:w-auto md:flex-grow min-w-[200px] mb-2 md:mb-0">
         <Input
           type="text"
           placeholder="🔎 Search posts..."
@@ -95,135 +95,137 @@ const ForumFilters = () => {
         />
       </div>
 
-      {isLoadingCategories ? (
-        <Skeleton className="h-9 w-40 rounded-md" />
-      ) : (
-        <Popover open={openCategory} onOpenChange={onOpenCategory}>
+      <div className="w-full md:w-auto flex flex-wrap gap-2">
+        {isLoadingCategories ? (
+          <Skeleton className="h-9 w-40 rounded-md" />
+        ) : (
+          <Popover open={openCategory} onOpenChange={onOpenCategory}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={openCategory}
+                className="flex w-fit items-center justify-between border-dashed hover:cursor-pointer gap-x-2"
+              >
+                <Icons.layers className=" h-4 w-4 shrink-0 opacity-50" />
+                <span>Category</span>
+                {categoryName && (
+                  <span className="text-xs text-muted-foreground">|</span>
+                )}
+                {categoryName && (
+                  <Badge className="bg-slate-900 hover:bg-slate-900 text-white">
+                    {searchParams.get('category') || categoryName}
+                  </Badge>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+              <Command>
+                <CommandInput placeholder="Search category..." />
+                <CommandEmpty>No framework found.</CommandEmpty>
+                <CommandGroup>
+                  {categoryOptions.map(category => (
+                    <CommandItem
+                      className="line-clamp-1"
+                      key={category.name}
+                      value={category.name}
+                      onSelect={() => {
+                        handleSetCategoryName(category.name)
+                        onOpenCategory(false)
+                        router.push(
+                          `/forum?category=${category.name}${
+                            searchParams.get('search')
+                              ? `&search=${searchParams.get('search')}`
+                              : ''
+                          }${
+                            searchParams.get('orderBy')
+                              ? `&orderBy=${searchParams.get('orderBy')}`
+                              : ''
+                          }`
+                        )
+                      }}
+                    >
+                      <Check
+                        className={cn(
+                          'mr-2 h-4 w-4',
+                          categoryName === category.name
+                            ? 'opacity-100'
+                            : 'opacity-0'
+                        )}
+                      />
+                      {category.name}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        )}
+
+        <Popover open={openOrderBy} onOpenChange={onOpenOrderBy}>
           <PopoverTrigger asChild>
             <Button
               variant="outline"
               role="combobox"
-              aria-expanded={openCategory}
-              className="flex w-fit items-center justify-between border-dashed hover:cursor-pointer gap-x-2"
+              aria-expanded={openOrderBy}
+              className="flex w-fit justify-between border-dashed hover:cursor-pointer gap-x-2"
             >
-              <Icons.layers className=" h-4 w-4 shrink-0 opacity-50" />
-              <span>Category</span>
-              {categoryName && (
+              <Icons.arrowUpDown className="h-4 w-4 shrink-0 opacity-50" />
+              <span>Order by</span>
+              {orderBy && (
                 <span className="text-xs text-muted-foreground">|</span>
               )}
-              {categoryName && (
+              {orderBy && (
                 <Badge className="bg-slate-900 hover:bg-slate-900 text-white">
-                  {searchParams.get('category') || categoryName}
+                  {orderBy}
                 </Badge>
               )}
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-[200px] p-0">
             <Command>
-              <CommandInput placeholder="Search category..." />
-              <CommandEmpty>No framework found.</CommandEmpty>
               <CommandGroup>
-                {categoryOptions.map(category => (
+                {orderByOptions.map(orderByOption => (
                   <CommandItem
                     className="line-clamp-1"
-                    key={category.name}
-                    value={category.name}
-                    onSelect={() => {
-                      handleSetCategoryName(category.name)
-                      onOpenCategory(false)
-                      router.push(
-                        `/forum?category=${category.name}${
-                          searchParams.get('search')
-                            ? `&search=${searchParams.get('search')}`
-                            : ''
-                        }${
-                          searchParams.get('orderBy')
-                            ? `&orderBy=${searchParams.get('orderBy')}`
-                            : ''
-                        }`
-                      )
+                    key={orderByOption.id}
+                    value={orderByOption.id}
+                    onSelect={currentValue => {
+                      handleSelect(currentValue)
+                      onOpenOrderBy(false)
                     }}
                   >
                     <Check
                       className={cn(
                         'mr-2 h-4 w-4',
-                        categoryName === category.name
+                        orderBy === orderByOption.value
                           ? 'opacity-100'
                           : 'opacity-0'
                       )}
                     />
-                    {category.name}
+                    <span>{orderByOption.value}</span>
+                    {orderByOption.icon}
                   </CommandItem>
                 ))}
               </CommandGroup>
             </Command>
           </PopoverContent>
         </Popover>
-      )}
 
-      <Popover open={openOrderBy} onOpenChange={onOpenOrderBy}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={openOrderBy}
-            className="flex w-fit justify-between border-dashed hover:cursor-pointer gap-x-2"
-          >
-            <Icons.arrowUpDown className="h-4 w-4 shrink-0 opacity-50" />
-            <span>Order by</span>
-            {orderBy && (
-              <span className="text-xs text-muted-foreground">|</span>
-            )}
-            {orderBy && (
-              <Badge className="bg-slate-900 hover:bg-slate-900 text-white">
-                {orderBy}
-              </Badge>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
-          <Command>
-            <CommandGroup>
-              {orderByOptions.map(orderByOption => (
-                <CommandItem
-                  className="line-clamp-1"
-                  key={orderByOption.id}
-                  value={orderByOption.id}
-                  onSelect={currentValue => {
-                    handleSelect(currentValue)
-                    onOpenOrderBy(false)
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      'mr-2 h-4 w-4',
-                      orderBy === orderByOption.value
-                        ? 'opacity-100'
-                        : 'opacity-0'
-                    )}
-                  />
-                  <span>{orderByOption.value}</span>
-                  {orderByOption.icon}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
-
-      <Button
-        onClick={() => {
-          handleSetCategoryName('All')
-          handleSetOrderBy('')
-          resetSearchTerm()
-          router.push(`/forum?category=All`)
-        }}
-        variant="ghost"
-        className="border-dashed border"
-      >
-        Reset
-        <Icons.close className="ml-2" size={15} />
-      </Button>
+        <Button
+          onClick={() => {
+            handleSetCategoryName('All')
+            handleSetOrderBy('')
+            resetSearchTerm()
+            router.push(`/forum?category=All`)
+          }}
+          variant="ghost"
+          className="border-dashed border"
+        >
+          Reset
+          <Icons.close className="ml-2" size={15} />
+        </Button>
+      </div>
     </div>
   )
 }

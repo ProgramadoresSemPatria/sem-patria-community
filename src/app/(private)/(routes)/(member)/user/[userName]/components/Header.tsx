ba163c0c405e24aa84921a4e6404d6897b554da9
Bernardo Bossi <bernardobossi77@gmail.com>
2024-11-24 13:21:22 +0000

    fix: delete comments

--- src/app/(private)/(routes)/(member)/user/[userName]/components/Header.tsx
+++ src/app/(private)/(routes)/(member)/user/[userName]/components/Header.tsx
@@ -3,10 +3,8 @@
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
 import { type User } from '@prisma/client'
 import { type User as ClerkUser } from '@clerk/nextjs/server'
-// import { useFollowersAndFollowings } from './useFollowersAndFollowings'
 
 const Header = ({
   user,
@@ -15,18 +13,6 @@ const Header = ({
   user: User
   currentUser: ClerkUser
 }) => {
-  //   const { follow, followers, unfollow, following, unfollowing } =
-  //     useFollowersAndFollowings(user.id)
-  //   const [isFollowed, setIsFollowed] = React.useState(false)
-
-  //   React.useEffect(() => {
-  //     if (!followers) return
-  //     const isFollowing = followers?.find(follower => {
-  //       return follower.id === currentUser.id
-  //     })
-  //     setIsFollowed(!!isFollowing)
-  //   }, [currentUser.id, followers])
-
   return (
     <div className="shadow-lg rounded-lg space-y-4">
       <div className="flex items-center gap-8 pb-6 border-b">
@@ -45,39 +31,18 @@ const Header = ({
             <Badge className="text-sm py-1 px-3">{user?.role[0]}</Badge>
           </h1>
           <p className="text-gray-500 text-lg">@{user?.username}</p>
-          {/* <Button
-            disabled={following || unfollowing}
-            onClick={async () => {
-              if (isFollowed) {
-                await unfollow(user.id)
-              } else {
-                await follow(user.id)
-              }
-            }}
-            variant="outline"
-            className="text-sm mt-2 hover:bg-gray-400"
-          >
-            {isFollowed ? 'Unfollow' : 'Follow'}
-          </Button> */}
-          <Button>Follow</Button>
         </div>
       </div>
       <div className="grid grid-cols-2 gap-6 text-center md:grid-cols-2">
         <div>
-          {/* <p className="text-2xl font-semibold">{followers.length}</p> */}
           <p className="text-2xl font-semibold">0</p>
 
           <p className="text-gray-500">Followers</p>
         </div>
         <div>
-          {/* <p className="text-2xl font-semibold">{user.followings}</p> */}
           <p className="text-2xl font-semibold">0</p>
           <p className="text-gray-500">Following</p>
         </div>
-        {/* <div>
-          <p className="text-2xl font-semibold">{points}</p>
-          <p className="text-gray-500">Points</p>
-        </div> */}
       </div>
     </div>
   )
--- src/app/(private)/(routes)/(member)/user/[userName]/page.tsx
+++ src/app/(private)/(routes)/(member)/user/[userName]/page.tsx
@@ -3,7 +3,6 @@ import prismadb from '@/lib/prismadb'
 import React from 'react'
 import UserPosts from './components/UserPosts'
 import Loading from '@/app/loading'
-// import UserInterests from './components/UserInterests'
 import SocialLinks from './components/SocialLinks'
 import Header from './components/Header'
 import { currentUser, type User } from '@clerk/nextjs/server'
@@ -35,13 +34,7 @@ const PublicProfile = async ({ params }: PublicProfileProps) => {
   return (
     <DefaultLayout>
       <Header user={profileUser} currentUser={user} />
-      {/* <UserInterests
-        userInterests={profileUser?.interests}
-        allInterests={interests}
-        userId={profileUser?.id}
-      /> */}
       <SocialLinks
-        // showEmail={profileUser.isPublicEmail || true}
         email={profileUser.email}
         linkedin={profileUser.linkedin || ''}
         github={profileUser.github || ''}

de570c649e03dce6f66f89b02315a1876dadaaa1
Bernardo Bossi <bernardobossi77@gmail.com>
2024-11-25 19:59:46 +0000

    feat: ui admin interests

---
+++ src/app/(private)/(routes)/admin/interests/page.tsx
@@ -0,0 +1,29 @@
+import { DefaultLayout } from '@/components/default-layout'
+import { SkeletonCmsPage } from '@/components/skeletons/skeleton-cms-page'
+import prismadb from '@/lib/prismadb'
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { Suspense } from 'react'
+import InterestsClient from './components/interest-client'
+
+const AdminInterestsPage = async () => {
+  const interests = await prismadb.interest.findMany()
+
+  const interestFormatted = interests.map(item => ({
+    id: item.id,
+    interest: item.interest,
+    createdAt: format(item.createdAt, 'dd-LL-yyyy', {
+      locale: ptBR
+    })
+  }))
+
+  return (
+    <DefaultLayout>
+      <Suspense fallback={<SkeletonCmsPage />}>
+        <InterestsClient interests={interestFormatted} />
+      </Suspense>
+    </DefaultLayout>
+  )
+}
+
+export default AdminInterestsPage

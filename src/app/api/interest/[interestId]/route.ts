b22feee199c49b935823f3947089fe6cf8183abb
Lucas Yule (GitHub) <lyrma@cin.ufpe.br>
2024-11-25 10:38:40 -0300

    chore: cleaning unused code at interest route

--- src/app/api/interest/[interestId]/route.ts
+++ src/app/api/interest/[interestId]/route.ts
@@ -7,10 +7,6 @@ export async function GET(req: NextRequest) {
     const { userId } = auth()
 
     if (!userId) return new NextResponse('Unauthenticated', { status: 401 })
-
-    // const interests = await prismadb.interest.findUnique({wher})
-
-    // return new NextResponse(JSON.stringify(interests), { status: 200 })
   } catch (error) {
     console.log('[CREATE_INTEREST_ERROR]', error)
     return new NextResponse('Internal Server Error', { status: 500 })

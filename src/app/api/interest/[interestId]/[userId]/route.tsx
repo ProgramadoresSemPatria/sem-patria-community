258f18b5b7c852cca2f3f48b9c81d30d85208653
Bernardo Bossi <bernardobossi77@gmail.com>
2024-12-02 21:02:48 +0000

    feat: omterests routes

--- src/app/api/interest/[interestId]/[userId]/route.tsx
+++ src/app/api/interest/[interestId]/[userId]/route.tsx
@@ -2,25 +2,59 @@ import prismadb from '@/lib/prismadb'
 import { auth } from '@clerk/nextjs/server'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function POST(req: NextRequest) {
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { interestId: string; userId: string } }
+) {
   try {
     const { userId } = auth()
-    const { interestId } = await req.json()
-
     if (!userId) return new NextResponse('Unauthenticated', { status: 401 })
+    if (!(params.userId === userId)) {
+      return new NextResponse('Don`t have permission', { status: 401 })
+    }
 
     await prismadb.user.update({
-      where: { id: userId },
+      where: { id: params.userId },
       data: {
         interests: {
-          connect: { id: interestId }
+          connect: { id: params.interestId }
         }
       }
     })
 
     return new NextResponse('Post deleted', { status: 200 })
   } catch (error) {
-    console.log('[CREATE_INTEREST_ERROR]', error)
+    console.log('[CONNECT_INTEREST_ERROR]', error)
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { interestId: string; userId: string } }
+) {
+  try {
+    const { userId } = auth()
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 })
+    }
+
+    if (!(params.userId === userId)) {
+      return new NextResponse('Don`t have permission', { status: 401 })
+    }
+    await prismadb.user.update({
+      where: { id: userId },
+      data: {
+        interests: {
+          disconnect: { id: params.interestId }
+        }
+      }
+    })
+
+    return new NextResponse('Interest removed successfully', { status: 200 })
+  } catch (error) {
+    console.error('[REMOVE_INTEREST_ERROR]', error)
     return new NextResponse('Internal Server Error', { status: 500 })
   }
 }

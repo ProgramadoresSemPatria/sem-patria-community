a85901363db530fe228e1916632a8d6f563f3c4f
Bernardo Bossi <bernardobossi77@gmail.com>
2025-01-26 12:35:19 +0000

    fix: env variables in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,18 @@
 import { clerkSetup } from '@clerk/testing/cypress'
 import { defineConfig } from 'cypress'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   screenshotOnRunFailure: false,
-  defaultCommandTimeout: 50000,
+  defaultCommandTimeout: 20000,
   e2e: {
     async setupNodeEvents(on, config) {
+      console.log('config', config)
       return await clerkSetup({ config })
     },
-    baseUrl: process.env.BASE_URL_PRODUCTION || process.env.BASE_URL_DEVELOPMENT
+    baseUrl: 'http://localhost:3000'
+    // baseUrl: process.env.BASE_URL_PRODUCTION || process.env.BASE_URL_DEVELOPMENT
   }
 })

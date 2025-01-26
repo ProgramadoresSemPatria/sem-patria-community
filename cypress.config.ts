9e542cc8e83945621d84b64545b4fa4c961b25e1
Bernardo Bossi <bernardobossi77@gmail.com>
2025-01-26 12:42:34 +0000

    fix: update env variables

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { clerkSetup } from '@clerk/testing/cypress'
 import { defineConfig } from 'cypress'
 import * as dotenv from 'dotenv'
 
+// Load environment variables from .env
 dotenv.config()
 
 export default defineConfig({
@@ -9,10 +10,22 @@ export default defineConfig({
   defaultCommandTimeout: 20000,
   e2e: {
     async setupNodeEvents(on, config) {
-      console.log('config', config)
+      config.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY =
+        process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+      config.env.BASE_URL_PRODUCTION = process.env.BASE_URL_PRODUCTION
+      config.env.BASE_URL_DEVELOPMENT = process.env.BASE_URL_DEVELOPMENT
+
+      console.log('Loaded Environment Variables:', {
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+          config.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+        BASE_URL_PRODUCTION: config.env.BASE_URL_PRODUCTION,
+        BASE_URL_DEVELOPMENT: config.env.BASE_URL_DEVELOPMENT
+      })
       return await clerkSetup({ config })
     },
-    // baseUrl: 'http://localhost:3000'
-    baseUrl: process.env.BASE_URL_PRODUCTION || process.env.BASE_URL_DEVELOPMENT
+    baseUrl:
+      process.env.NODE_ENV === 'production'
+        ? process.env.BASE_URL_PRODUCTION
+        : process.env.BASE_URL_DEVELOPMENT
   }
 })

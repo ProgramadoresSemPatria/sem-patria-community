c207f58edcf4b8d961507a85452c724b754d72e8
Bernardo Bossi <bernardobossi77@gmail.com>
2025-02-22 12:10:01 +0000

    fix: remove variables from cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,15 +7,9 @@ dotenv.config()
 export default defineConfig({
   screenshotOnRunFailure: false,
   defaultCommandTimeout: 20000,
-  env: {
-    CLERK_TESTING_TOKEN: process.env.CLERK_TESTING_TOKEN,
-    TEST_EMAIL: process.env.TEST_EMAIL || 'lucasyule6@gmail.com',
-    TEST_PASSWORD: process.env.TEST_PASSWORD || 'lucasyule'
-  },
   e2e: {
     async setupNodeEvents(on, config) {
       return await clerkSetup({ config })
-    },
-    baseUrl: 'http://localhost:3000/'
+    }
   }
 })

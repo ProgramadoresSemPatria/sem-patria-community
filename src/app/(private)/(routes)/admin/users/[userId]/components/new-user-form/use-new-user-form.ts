592a4567d9b6c1a59551856ef7dfbfcdc389f5e9
Lucas Yule (GitHub) <lucasyule6@gmail.com>
2025-02-25 09:55:02 -0300

    refactor: removing obsolete roles references

--- src/app/(private)/(routes)/admin/users/[userId]/components/new-user-form/use-new-user-form.ts
+++ src/app/(private)/(routes)/admin/users/[userId]/components/new-user-form/use-new-user-form.ts
@@ -33,16 +33,7 @@ const formSchema = z.object({
       message: 'Email is required'
     }),
   role: z.array(
-    z.enum([
-      'PrePsp',
-      'PerfilFechado',
-      'PortifolioBoostProgram',
-      'Base',
-      'ProgramadorSemPatria',
-      'Prime',
-      'Builder',
-      'Admin'
-    ])
+    z.enum(['PrePsp', 'Base', 'ProgramadorSemPatria', 'Prime', 'Admin'])
   ),
   level: z.string(),
   github: z.string(),
@@ -76,27 +67,27 @@ export const useNewUserForm = ({ initialData }: UseNewUserFormProps) => {
     resolver: zodResolver(formSchema),
     defaultValues: initialData
       ? {
-          name: initialData.name || '',
-          username: initialData.username || '',
-          email: initialData.email || '',
-          role: initialData.role || [],
-          level: initialData.level || '',
-          github: initialData.github || '',
-          linkedin: initialData.linkedin || '',
-          instagram: initialData.instagram || '',
-          position: initialData.position || undefined
-        }
+        name: initialData.name || '',
+        username: initialData.username || '',
+        email: initialData.email || '',
+        role: initialData.role || [],
+        level: initialData.level || '',
+        github: initialData.github || '',
+        linkedin: initialData.linkedin || '',
+        instagram: initialData.instagram || '',
+        position: initialData.position || undefined
+      }
       : {
-          name: '',
-          username: '',
-          email: '',
-          role: [],
-          level: '',
-          github: '',
-          linkedin: '',
-          instagram: '',
-          position: undefined
-        }
+        name: '',
+        username: '',
+        email: '',
+        role: [],
+        level: '',
+        github: '',
+        linkedin: '',
+        instagram: '',
+        position: undefined
+      }
   })
 
   const { mutateAsync: deleteUser, isPending: isDeletingUser } = useMutation({
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -59,7 +59,6 @@ export const formatTitle = (title: string) => {
     'a-base': 'A Base',
     psp: 'Programador Sem Pátria',
     prime: 'Programador Prime',
-    'perfil-fechado': 'Perfil Fechado'
   }
 
   return titleHref[title]
@@ -90,10 +89,8 @@ export const getStringFromDate = (date: string) => {
 
 export const defineUserRole = (user: User): UserAbilityRoles => {
   const hasAdmin = user.role.some(role => role === Roles.Admin)
-  const hasBuilder = user.role.some(role => role === Roles.Builder)
 
   if (hasAdmin) return 'ADMIN'
-  if (hasBuilder) return 'BUILDER'
   return 'MEMBER'
 }
 

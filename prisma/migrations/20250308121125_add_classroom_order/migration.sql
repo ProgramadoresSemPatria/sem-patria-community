cff2433a345ea813d2cb50d4515cdfc2e27c624a
carlosdevv <carloslopessf@gmail.com>
2025-03-19 14:10:10 -0300

    fix: migration prisma

--- prisma/migrations/20250308121125_add_classroom_order/migration.sql
+++ prisma/migrations/20250308121125_add_classroom_order/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE "Classroom" ADD COLUMN     "order" INTEGER;
 
 -- AlterTable
-ALTER TABLE "_InterestToUser" ADD CONSTRAINT "_InterestToUser_AB_pkey" PRIMARY KEY ("A", "B");
+-- ALTER TABLE "_InterestToUser" ADD CONSTRAINT "_InterestToUser_AB_pkey" PRIMARY KEY ("A", "B");
 
 -- DropIndex
 DROP INDEX "_InterestToUser_AB_unique";

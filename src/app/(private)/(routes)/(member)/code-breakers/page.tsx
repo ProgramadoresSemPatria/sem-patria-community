c5124186f9c50ded62190cac966e0304392965b8
Lucas Yule (GitHub) <lucasyule6@gmail.com>
2025-03-18 22:53:34 -0300

    fix: fixing server component issue + window is not defined issue

--- src/app/(private)/(routes)/(member)/code-breakers/page.tsx
+++ src/app/(private)/(routes)/(member)/code-breakers/page.tsx
@@ -1,16 +1,11 @@
-'use client'
-
 import { DefaultLayout } from '@/components/default-layout'
-import { useEffect } from "react";
-import "@xyflow/react/dist/style.css";
+import '@xyflow/react/dist/style.css'
 import { TreeMap } from './components/TreeMap'
 
-
 const CodeBreakersPage = () => {
-
   return (
     <DefaultLayout className="max-w-none mx-0 px-0 pt-0">
-      <TreeMap/>
+      <TreeMap />
     </DefaultLayout>
   )
 }

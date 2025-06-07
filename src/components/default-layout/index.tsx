95cf5d1a4f0553f82caf9bbb714fe3c6e7228ac1
Bernardo Bossi <bernardobossi77@gmail.com>
2025-06-07 13:40:41 +0100

    feat: only call currentUser method if no cookies

--- src/components/default-layout/index.tsx
+++ src/components/default-layout/index.tsx
@@ -5,27 +5,36 @@ import { AppSidebar } from '../app-sidebar'
 import { currentUser } from '@clerk/nextjs/server'
 import Loading from '@/app/loading'
 import prismadb from '@/lib/prismadb'
+import { cookies } from 'next/headers'
 
 type DefaultLayoutProps = {
   children: React.ReactNode
   className?: string
 }
 
+export const ensureUsernameCookie = async () => {
+  const cookieStore = cookies()
+  const existing = cookieStore.get('x-username')
+  if (existing) return existing.value
+  const user = await currentUser()
+  if (!user?.id) return null
+  const username =
+    user.username ||
+    (
+      await prismadb.user.findUnique({
+        where: { id: user.id },
+        select: { username: true }
+      })
+    )?.username
+  if (!username) return null
+  return username
+}
+
 export const DefaultLayout = async ({
   children,
   className
 }: DefaultLayoutProps) => {
-  const clerkUser = await currentUser()
-  if (!clerkUser) return <Loading />
-
-  const username =
-    clerkUser.username ??
-    (
-      await prismadb.user.findUnique({
-        where: { id: clerkUser.id }
-      })
-    )?.username
-
+  const username = await ensureUsernameCookie()
   if (!username) return <Loading />
 
   return (

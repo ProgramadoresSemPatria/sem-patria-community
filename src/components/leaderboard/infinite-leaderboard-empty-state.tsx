a20a7d93f0fef3e83d2fbbd7a75c435aae354ae4
Igor Alves <igorsk89@gmail.com>
2025-06-10 02:59:19 -0300

    style: add padding and center text in empty state component

--- src/components/leaderboard/infinite-leaderboard-empty-state.tsx
+++ src/components/leaderboard/infinite-leaderboard-empty-state.tsx
@@ -4,9 +4,9 @@ import { Icons } from '@/components/icons'
 
 export const InfiniteLeaderboardEmptyState = () => {
   return (
-    <div className="flex-1 flex flex-col items-center justify-center py-8 mt-2 rounded-lg">
+    <div className="flex-1 flex flex-col items-center justify-center py-8 mt-2 rounded-lg px-4">
       <Icons.trophy className="w-16 h-16 text-muted-foreground" />
-      <h3 className="text-lg font-medium text-muted-foreground mt-4">
+      <h3 className="text-lg font-medium text-muted-foreground mt-4 text-center">
         Legends are being made! Will you be next?
       </h3>
       <p className="text-sm text-muted-foreground/70 mt-1 max-w-md text-center">

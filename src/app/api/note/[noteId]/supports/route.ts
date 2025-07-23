ee9cb5ee0628317006aa95a79314549771c08b49
Pedro Oliveira <pedrogagodev@gmail.com>
2025-07-23 18:58:38 -0300

    refactor: update import in PATCH route for note support for use NextRequest as type

--- src/app/api/note/[noteId]/supports/route.ts
+++ src/app/api/note/[noteId]/supports/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 import { auth } from '@clerk/nextjs/server'
 import prismadb from '@/lib/prismadb'
 
